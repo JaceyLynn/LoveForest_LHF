2b278481fd8ddfb44e88c32d006dd65f4a89a7ab
JaceyLynn <jqz0807@gmail.com>
2026-03-19 03:40:46 -0400

    Fix Vercel 404: simplify config, defer env check to runtime

--- api/lib/mongodb.js
+++ api/lib/mongodb.js
@@ -1,13 +1,5 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-    throw new Error('MONGODB_URI environment variable is not set');
-}
-
-const options = {};
-
 let cachedClient = null;
 let cachedDb = null;
 
@@ -16,7 +8,12 @@ export async function connectToDatabase() {
         return { client: cachedClient, db: cachedDb };
     }
 
-    const client = new MongoClient(uri, options);
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+        throw new Error('MONGODB_URI environment variable is not set');
+    }
+
+    const client = new MongoClient(uri);
     await client.connect();
     const db = client.db('lhf_forest');
 
