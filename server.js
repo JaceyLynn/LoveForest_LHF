39f3b63d0bd1a3fc05d2d9497b00bce22f9b46cb
JaceyLynn <jqz0807@gmail.com>
2026-03-27 16:33:31 -0400

    Fix card layout, mobile responsive, nav links styling

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ async function connectMongo() {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Endpoint to get encouragement from ChatGPT
 app.post('/api/encourage', async (req, res) => {
